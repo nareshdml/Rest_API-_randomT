9f98097fc6bda41c206312cb0ee2c0fb967ce95d
Naresh Dhimal <dreamnarus99@gmail.com>
2023-07-03 11:16:06 +0545

    RestAPI randomUser

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:rest_api/screen/homeScreen.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'RestApi',
+      home: HomeScreen(),
+    );
+  }
+}
